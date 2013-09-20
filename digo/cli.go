130e41eea4a7acb46859e29e0885b18901262e0d
Tobias Schwab <tobias.schwab@dynport.de>
2013-09-20 13:44:37 +0200

    only print revision when defined

--- digo/cli.go
+++ digo/cli.go
@@ -8,17 +8,19 @@ import (
 
 func init() {
 	cli.Register("version",
-		&gocli.Action {
+		&gocli.Action{
 			Description: "Print version and revision",
-			Handler: VersionAction,
+			Handler:     VersionAction,
 		},
-		)
+	)
 }
 
 func VersionAction(args *gocli.Args) error {
 	table := gocli.NewTable()
 	table.Add("Version", digo.VERSION)
-	table.Add("Revision", digo.GITCOMMIT)
+	if len(digo.GITCOMMIT) > 0 {
+		table.Add("Revision", digo.GITCOMMIT)
+	}
 	fmt.Printf(table.String())
 	return nil
 }
